9207ee44400062249774dd2ade8a8946e5abf4ad
Ishboi <bolachaseleite@gmail.com>
2019-10-17 19:50:11 +0100

    EnhancedStatements

---
+++ EnhancedIfStatementC/EnhancedIfStatementC/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnhancedIfStatementC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int temperature = -5;
+            string stateOfMatter;
+            /*
+            if(temperature < 0)
+            {
+                stateOfMatter = "solid";
+            }
+            else
+            {
+                stateOfMatter = "liquid";
+            } */
+
+
+            // in short:
+
+            temperature += 20;
+            
+            stateOfMatter = temperature > 100 ? "gas" : temperature < 0 ? "solid" : "liquid";
+            Console.WriteLine("State of matter is {0}", stateOfMatter);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
