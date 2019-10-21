28df4e84077fd45bd7f262839e0c718dc4220b6e
Ishboi <bolachaseleite@gmail.com>
2019-10-21 15:45:53 +0100

    odd for loop

--- ForLoop/ForLoop/Program.cs
+++ ForLoop/ForLoop/Program.cs
@@ -10,6 +10,24 @@ namespace ForLoop
     {
         static void Main(string[] args)
         {
+
+
+            for(int counter = 1; counter < 50; counter++)
+            {
+                if(counter % 2 > 0)
+                {
+                    Console.WriteLine(counter + " is odd");
+                }
+            }
+            for (int counter = 1; counter < 20; counter+=2)
+            {
+                if(counter % 2 > 0)
+                {
+                    Console.WriteLine(counter + " is odd");
+                }
+            }
+            Console.WriteLine("For loop is done");
+            Console.Read();
         }
     }
 }
