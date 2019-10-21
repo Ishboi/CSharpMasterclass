713f473c6fa839b9d531e28586902aa42ceeed4d
Ishboi <bolachaseleite@gmail.com>
2019-10-21 19:19:48 +0100

    Introduction to properties set and get

---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(5, 4, 10);
+            Console.WriteLine("volume is{0}", box.Volume);
+            
+
+            box.DisplayInfo();
+            Console.ReadKey();
+        }
+    }
+}
