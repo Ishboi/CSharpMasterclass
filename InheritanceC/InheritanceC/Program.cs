265cfafe0f85309096bec61e027ccf75384f5f4f
Ishboi <bolachaseleite@gmail.com>
2019-10-25 13:11:06 +0100

    Inheritance introduction

---
+++ InheritanceC/InheritanceC/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Post post1 = new Post("Thanks for the birthday wishes", true, "Fabio C");
+            Console.WriteLine(post1.ToString());
+            ImagePost imagePost1 = new ImagePost("Check out my new shoes", "Fabio C", 
+                "https://images.com/shoes", true);
+            Console.WriteLine(imagePost1.ToString());
+            Console.ReadKey();
+
+
+        }
+    }
+}
