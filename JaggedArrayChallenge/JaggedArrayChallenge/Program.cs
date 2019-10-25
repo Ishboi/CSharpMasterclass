ebe8b2e192339a0eddfc6ad07d420becdf5af3ca
Ishboi <bolachaseleite@gmail.com>
2019-10-25 11:34:53 +0100

    jagged arrays challenge with friends and family to present to

---
+++ JaggedArrayChallenge/JaggedArrayChallenge/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArrayChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[][] friends = new string[][] {
+                new string[] {"sandra", "pai", "tobias"},
+                new string[] {"daniel", "teresa", "Sao"},
+                new string[] {"raquel", "andre", "sandra2"}
+            };
+
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("Type exit to quit.");
+                Console.Write("Introduce who > ");
+                string friend1 = Console.ReadLine();
+                Console.Write(" to whom > ");
+                string friend2 = Console.ReadLine();
+                if (friend1.ToUpper().Equals("EXIT") || friend2.ToUpper().Equals("EXIT"))
+                {
+                    return;
+                }
+                if (!findFriend(friends, friend1) || !findFriend(friends,friend2))
+                {
+                    Console.WriteLine("Friend doesn't exist. Please try again.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+                Console.WriteLine("{0} this is {1}", friend1, friend2);
+                Console.ReadKey();
+            }
+        }
+        public static bool findFriend(string[][] friends, string friend)
+        {
+
+            for (int i = 0; i < friends.Length; i++)
+            {
+                foreach(string value in friends[i])
+                {
+                    if(friend.ToUpper().Equals(value.ToUpper()))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+
+
+
+}
