f01b768d6bf2dcb817c21880a9c18ce9d9708cac
Ishboi <bolachaseleite@gmail.com>
2019-10-30 15:02:40 +0000

    random class

---
+++ RandomT/RandomT/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random dice = new Random();
+            int numEyes;
+
+            for(int i = 0; i<10; i++)
+            {
+                numEyes = dice.Next(1,7);
+                Console.WriteLine(numEyes);
+            }
+            Console.WriteLine("Please enter your question.");
+            Console.ReadLine();
+            numEyes = dice.Next(1, 4); //between the range where you want to start and +1 where you want to end, so it's 3 but insert 4 instead
+            switch(numEyes)
+            {
+                case 1:
+                    Console.WriteLine("Yes");
+                    break;
+                case 2:
+                    Console.WriteLine("Maybe");
+                    break;
+                case 3:
+                    Console.WriteLine("No");
+                    break;
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
--- StructC/StructC/Program.cs
+++ StructC/StructC/Program.cs
@@ -7,12 +7,20 @@ using System.Threading.Tasks;
 namespace StructC
 {
 
-    struct Game
+    public struct Game
     {
         public string name;
         public string developer;
         public double rating;
         public string releaseDate;
+
+        public Game(string name, string developer, double rating, string releaseDate)
+        {
+            this.name = name;
+            this.developer = developer;
+            this.rating = rating;
+            this.releaseDate = releaseDate;
+        }
 
         public void Display()
         {
@@ -20,6 +28,7 @@ namespace StructC
             Console.WriteLine("Game 1 was developed by : {0}", developer);
             Console.WriteLine("Game 1's rating is : {0}", rating);
             Console.WriteLine("Game 1 was released in: {0}", releaseDate);
+            
         }
 
     }
