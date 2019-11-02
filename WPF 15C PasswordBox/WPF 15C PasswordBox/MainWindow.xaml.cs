073b79251ded000998cd639a9516f86b6b581775
Ishboi <bolachaseleite@gmail.com>
2019-11-02 16:09:56 +0000

    Password box, changing max length, the chars that appear also

--- WPF 15C PasswordBox/WPF 15C PasswordBox/MainWindow.xaml.cs
+++ WPF 15C PasswordBox/WPF 15C PasswordBox/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPF_15C_PasswordBox
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Welcome " + tbUsername.Text);
+        }
     }
 }
