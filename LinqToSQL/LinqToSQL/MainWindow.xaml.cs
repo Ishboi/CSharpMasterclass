6f74185787bad79733995c50c2c39f6dddc05550
Ishboi <bolachaseleite@gmail.com>
2019-11-11 12:17:23 +0000

    adding data grid from database, using lambda expressions with a list to insert data

---
+++ LinqToSQL/LinqToSQL/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Configuration;
+
+namespace LinqToSQL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        LinqToSqlDataClassesDataContext dataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["LinqToSQL.Properties.Settings.FabioCsharpDBConnectionString"].ConnectionString;
+            dataContext = new LinqToSqlDataClassesDataContext(connectionString);
+
+            //InsertUniversities();
+            InsertStudents();
+
+        }
+
+        public void InsertStudents()
+        {
+            University yale = dataContext.Universities.First(un => un.Name.Equals("Yale"));
+            // the above is the same as below with the lambda(=>) expression
+            //"from university in dataContext.University where university == "Yale" select university"
+            University ist = dataContext.Universities.First(un => un.Name.Equals("ist"));
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { Name = "Carla", Gender = "Female", UniversityId = yale.Id });
+            students.Add(new Student { Name = "Tonie", Gender = "Male", University = yale });
+            students.Add(new Student { Name = "Leyle", Gender = "Female", University = ist });
+            students.Add(new Student { Name = "Jame", Gender = "trans", University = ist });
+
+            dataContext.Students.InsertAllOnSubmit(students);
+            dataContext.SubmitChanges();
+            MainDataGrid.ItemsSource = dataContext.Students;
+
+        }
+
+        public void InsertUniversities()
+        {
+            dataContext.ExecuteCommand("delete from University");
+            University yale = new University();
+            yale.Name = "Yale";
+            dataContext.Universities.InsertOnSubmit(yale);
+            University ist = new University();
+            ist.Name = "IST";
+            dataContext.Universities.InsertOnSubmit(ist);
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Universities;
+        }
+
+    }
+}
