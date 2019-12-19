3f6bfb1e805b349f70c558608e296181bd869178
Ishboi <bolachaseleite@gmail.com>
2019-12-19 15:46:49 +0000

    Debugger concluded

--- DebuggingIntro/DebuggingIntro/Program.cs
+++ DebuggingIntro/DebuggingIntro/Program.cs
@@ -11,7 +11,8 @@ namespace DebuggingIntro
         static void Main(string[] args)
         {
             var friends = new List<string> { "Frank", "Joe", "Michelle", "Andy", "Maria", "Carlos", "Angelina" };
-            var partyFriends = GetPartyFriends(friends, 4);
+            //var friends = new List<string>();
+            var partyFriends = GetPartyFriends(friends, 10);
 
             foreach(var name in partyFriends)
             {
@@ -22,18 +23,35 @@ namespace DebuggingIntro
 
         public static List<string> GetPartyFriends(List<string> list, int count)
         {
+            if(list == null)
+            {
+                throw new ArgumentNullException("List", "The list is empty");
+            }
+            if(count > list.Count || count <=0)
+            {
+                throw new ArgumentOutOfRangeException("count", "Count cannot be greater than elements in the list");
+            }
+            var buffer = new List<string>(list);
             var partyFriends = new List<string>();
             while(partyFriends.Count < count)
             {
-                var currentFriend = GetPartyFriend(list);
+                var currentFriend = GetPartyFriend(buffer);
                 partyFriends.Add(currentFriend);
                 list.Remove(currentFriend);
+                buffer.Remove(currentFriend);
             }
             return partyFriends;
         }
 
+        /// <summary>
+        /// This is the logic to figure out who is a party friend
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+
         public static string GetPartyFriend(List<string> list)
         {
+
             string shortestName = list[0];
             for(var i = 0; i<list.Count; i++)
             {
