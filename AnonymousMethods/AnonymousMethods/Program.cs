3b556655a14ccc97688592bdc1e27997041a00a2
Ishboi <bolachaseleite@gmail.com>
2019-12-19 17:27:14 +0000

    anonymous methods

---
+++ AnonymousMethods/AnonymousMethods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethods
+{
+    class Program
+    {
+        public delegate string GetTextDelegate(string name);
+
+
+
+        static void Main(string[] args)
+        {
+            GetTextDelegate getTextDelegate = delegate (string name)
+            {
+                return "Hello, " + name;
+            };
+            Console.WriteLine(getTextDelegate("Denis"));
+            Display(getTextDelegate);
+            SayHello();
+            Console.ReadKey();
+        }
+
+        static void Display(GetTextDelegate getTextDelegate)
+        {
+            Console.WriteLine(getTextDelegate("World"));
+        }
+
+
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
