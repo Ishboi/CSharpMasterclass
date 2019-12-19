8df6028c51643f9c6d6d5f2e4a75815301153020
Ishboi <bolachaseleite@gmail.com>
2019-12-19 17:40:47 +0000

    Lambda expressions and statements

--- AnonymousMethods/AnonymousMethods/Program.cs
+++ AnonymousMethods/AnonymousMethods/Program.cs
@@ -10,18 +10,47 @@ namespace AnonymousMethods
     {
         public delegate string GetTextDelegate(string name);
 
-
+        public delegate double GetResultDelegate(double a, double b);
 
         static void Main(string[] args)
         {
+
+            // Creating an anonymous method
             GetTextDelegate getTextDelegate = delegate (string name)
             {
                 return "Hello, " + name;
             };
-            Console.WriteLine(getTextDelegate("Denis"));
-            Display(getTextDelegate);
+
+            // Lambda expression
+            GetTextDelegate getHelloText = (string name) => { return "Hello, " + name; };
+
+            // Statement lambda
+            GetTextDelegate getGoodbyeText = (string name) =>
+            {
+                Console.WriteLine("I'm inside of a lambda statement");
+                return "Goodbye, " + name;
+            };
+
+            // even nicer lambda expression, also it only works because it's just one variable the "name" variable
+            GetTextDelegate getWelcomeText = name => "Welcome, " + name;
+
+            // lambda expression with various variables
+            GetResultDelegate getSum = (a, b) => a + b;
+            GetResultDelegate getProduct = (a, b) => a * b;
+
+            DisplayNum(getSum);
+            DisplayNum(getProduct);
+
+
+            Console.WriteLine(getWelcomeText("Denis"));
+            //Display(getTextDelegate);
             SayHello();
             Console.ReadKey();
+        }
+
+        static void DisplayNum(GetResultDelegate getResultDelegate)
+        {
+            Console.WriteLine(getResultDelegate(3.5, 2.5));
         }
 
         static void Display(GetTextDelegate getTextDelegate)
