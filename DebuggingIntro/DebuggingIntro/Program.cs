905609e0575ab447edc11c59dbb4553adf3f8c6f
Ishboi <bolachaseleite@gmail.com>
2019-12-19 13:00:07 +0000

    Debugging pt2

--- DebuggingIntro/DebuggingIntro/Program.cs
+++ DebuggingIntro/DebuggingIntro/Program.cs
@@ -10,6 +10,40 @@ namespace DebuggingIntro
     {
         static void Main(string[] args)
         {
+            var friends = new List<string> { "Frank", "Joe", "Michelle", "Andy", "Maria", "Carlos", "Angelina" };
+            var partyFriends = GetPartyFriends(friends, 4);
+
+            foreach(var name in partyFriends)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadKey();
         }
+
+        public static List<string> GetPartyFriends(List<string> list, int count)
+        {
+            var partyFriends = new List<string>();
+            while(partyFriends.Count < count)
+            {
+                var currentFriend = GetPartyFriend(list);
+                partyFriends.Add(currentFriend);
+                list.Remove(currentFriend);
+            }
+            return partyFriends;
+        }
+
+        public static string GetPartyFriend(List<string> list)
+        {
+            string shortestName = list[0];
+            for(var i = 0; i<list.Count; i++)
+            {
+                if(list[i].Length < shortestName.Length)
+                {
+                    shortestName = list[i];
+                }
+            }
+            return shortestName;
+        }
+
     }
 }
